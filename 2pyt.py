921109f55468a55bf152292ed662a6a698c8ddd3
arpithmonteiro <57298318+arpithmonteiro@users.noreply.github.com>
2021-08-21 17:52:28 +0530

    Add files via upload

---
+++ 2pyt.py
@@ -0,0 +1,13 @@
+list=['sdd','sdsd',1]
+
+list.append('a')
+print(list)
+
+string1='arpith'
+re=''.join(sorted(string1))
+print(re)
+X=[x**2 for x in range(0,1) ]
+Y=[float(x) for x in range(0,10) if x==0]
+print (Y)
+print("Welcome to ",end="" ),
+print("GeeksforGeeks")
