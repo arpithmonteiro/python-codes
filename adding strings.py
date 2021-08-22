72acc1988057eaa61e475d61d11fcbed08fb7ef9
arpithmonteiro <57298318+arpithmonteiro@users.noreply.github.com>
2021-08-22 17:11:26 +0530

    Create adding strings.py

---
+++ adding strings.py
@@ -0,0 +1,14 @@
+
+# Python program to show use of
+# + operator for different purposes.
+ 
+print(1 + 2)
+ 
+# concatenate two strings
+print("Geeks"+"For")
+ 
+# Product two numbers
+print(3 * 4)
+ 
+# Repeat the String
+print("Geeks"*4)
